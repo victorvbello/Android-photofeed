2b5f0c5ccf230afe956f787dcbfe90e9f2e21783
Victor Bello <Victor Bello@localhost>
2016-10-14 12:35:16 -0400

    cambio de api de google ya se ve el mapa

--- app/src/main/java/com/example/victorbello/photofeed/photomap/di/PhotoMapComponent.java
+++ app/src/main/java/com/example/victorbello/photofeed/photomap/di/PhotoMapComponent.java
@@ -7,14 +7,15 @@ package com.example.victorbello.photofeed.photomap.di;
 
 import com.example.victorbello.photofeed.PhotoFeedAppModule;
 import com.example.victorbello.photofeed.domain.di.DomainModule;
-import com.example.victorbello.photofeed.photomap.PhotoMapPresenter;
+import com.example.victorbello.photofeed.lib.di.LibsModule;
+import com.example.victorbello.photofeed.photomap.ui.PhotoMapView;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules={PhotoMapModule.class, DomainModule.class, PhotoFeedAppModule.class})
+@Component(modules={PhotoMapModule.class, DomainModule.class, LibsModule.class,PhotoFeedAppModule.class})
 public interface PhotoMapComponent {
-    void inject(PhotoMapPresenter fragment);
+    void inject(PhotoMapView fragment);
 }
--- app/src/main/java/com/example/victorbello/photofeed/photomap/ui/PhotoMapFragment.java
+++ app/src/main/java/com/example/victorbello/photofeed/photomap/ui/PhotoMapFragment.java
@@ -10,12 +10,21 @@ import android.support.annotation.Nullable;
 import java.util.HashMap;
 
 import android.os.Bundle;
+import android.os.Build;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.support.design.widget.Snackbar;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import com.example.victorbello.photofeed.PhotoFeedApp;
 import com.example.victorbello.photofeed.R;
@@ -23,13 +32,18 @@ import com.example.victorbello.photofeed.entities.Photo;
 import com.example.victorbello.photofeed.photomap.PhotoMapPresenter;
 
 
-public class PhotoMapFragment extends Fragment implements PhotoMapView {
+
+public class PhotoMapFragment extends Fragment implements PhotoMapView , OnMapReadyCallback{
 
     private FrameLayout container;
     private HashMap<String,Integer> arrMsg;
+    private GoogleMap map;
+    private static final int PERMISSIONS_REQUEST_LOCATION=1;
 
+    /*
     @Inject
     public PhotoMapPresenter presenter;
+    */
 
 
     public PhotoMapFragment(){
@@ -43,18 +57,19 @@ public class PhotoMapFragment extends Fragment implements PhotoMapView {
     public void onCreate(@Nullable Bundle savedInstanceEstate){
         super.onCreate(savedInstanceEstate);
         setupInjection();
-        presenter.onCreate();
+        //presenter.onCreate();
     }
 
     private void setupInjection() {
         PhotoFeedApp app=(PhotoFeedApp) getActivity().getApplication();
+        app.getPhotoMapComponent(this,this).inject(this);
     }
 
 
     @Override
     public void onDestroy(){
-        presenter.unsubscribe();
-        presenter.onDestroy();
+        //presenter.unsubscribe();
+        //presenter.onDestroy();
         super.onDestroy();
     }
 
@@ -62,8 +77,17 @@ public class PhotoMapFragment extends Fragment implements PhotoMapView {
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view=inflater.inflate(R.layout.fragment_photo_map,container,false);
         container=(FrameLayout) view.findViewById(R.id.container);
-        presenter.subscribe();
+
+       //presenter.subscribe();
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm=getChildFragmentManager();
+        SupportMapFragment mapFragment=(SupportMapFragment) fm.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -85,6 +109,35 @@ public class PhotoMapFragment extends Fragment implements PhotoMapView {
             Snackbar.make(container, arrMsg.get(idMsg), Snackbar.LENGTH_SHORT).show();
         }else{
             Snackbar.make(container, getString(R.string.photolist_message_list_empty), Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        if(ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSIONS_REQUEST_LOCATION);
+            }
+            return;
+        }
+        map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults){
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_LOCATION:{
+                if(grantResults.length>0
+                        && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    if(map!=null){
+                        map.setMyLocationEnabled(true);
+                    }
+                }
+                return;
+            }
         }
     }
 }
